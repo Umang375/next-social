d0fe07031e6b3b2a7fe245c51e35c7f3ebfec112
Umang375 <ujain7315@gmail.com>
2024-08-15 14:42:39 +0530

    added localhost url and clerk webhooks

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,11 +1,45 @@
-import UserInfoCard from "@/components/rightMenu/UserInfoCard";
-import UserMediaCard from "@/components/rightMenu/UserMediaCard";
 import Feed from "@/components/feed/Feed";
 import RightMenu from "@/components/rightMenu/RightMenu";
 import LeftMenu from "@/components/leftMenu/LeftMenu";
 import Image from "next/image";
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
 
-const Homepage = () => {
+const ProfilePage = async ({params}:{params:{username:string}}) => {
+  const username = params.username;
+  const user = await prisma.user.findFirst({
+    where:{
+      username: username,
+    },
+    include:{
+      _count:{
+        select:{
+          followers: true,
+          followings: true,
+          posts: true,
+        }
+      }
+    }
+  })
+  if(!user) return <div>User not found</div>
+
+  const {userId : currentUserId} = auth();
+  let isBlocked = false;
+  if(currentUserId){
+    const res = await prisma.block.findFirst({
+      where:{
+        blockerId: user.id,
+        blockedId: currentUserId,
+      }
+    })
+
+    if(res) isBlocked = true;
+  }
+
+  if(isBlocked) return notFound();
+
+
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
@@ -15,29 +49,31 @@ const Homepage = () => {
         <div className="flex flex-col gap-6">
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
-              <Image src="https://images.pexels.com/photos/27308313/pexels-photo-27308313/free-photo-of-an-aerial-view-of-a-road-and-a-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={128} height={128}
+              <Image src={user.cover || "/noCover.png"}alt="" width={128} height={128}
               className=" w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover" />
-              <Image src="https://images.pexels.com/photos/27520303/pexels-photo-27520303/free-photo-of-a-woman-sitting-in-the-grass-holding-a-book.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" fill className="rounded-md object-cover" />
+              <Image src={user.avatar || "/noAvatar.png"} alt="" fill className="rounded-md object-cover" />
             </div>
             <h1 className="mt-20 mb-4 text-2xl font-medium">
-              ELva Weaver
+            {user.name && user.surname
+                ? user.name + " " + user.surname
+                : user.username}
             </h1>
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">{user._count.posts}</span>
                 <span className="text-sm">Posts</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">{user._count.followers}</span>
                 <span className="text-sm">Followers</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">123</span>
+                <span className="font-medium">{user._count.followings}</span>
                 <span className="text-sm">Following</span>
               </div>
             </div>
           </div>
-          <Feed />
+          <Feed username={user.username}/>
         </div>
       </div> 
       <div className="hidden lg:block w-[30%]">
@@ -47,4 +83,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default ProfilePage;
